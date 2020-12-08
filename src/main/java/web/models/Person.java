1c32d83cabdffb80f0d06a94ba3f3557b7255092
rudikone <mudoda51>
2020-12-08 17:06:12 +0300

    Версия без подключения к БД

--- src/main/java/web/models/Person.java
+++ src/main/java/web/models/Person.java
@@ -1,7 +1,13 @@
 package web.models;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class Person {
     private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
 
     public Person(){
