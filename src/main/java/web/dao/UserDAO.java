df7141f4f4e933380054cfb60532d5091da53cf6
rudikone <mudoda51>
2020-12-12 14:22:00 +0300

    MVC Version 1.0

--- src/main/java/web/dao/UserDAO.java
+++ src/main/java/web/dao/UserDAO.java
@@ -7,8 +7,12 @@ import java.util.List;
 public interface UserDAO {
 
     public List<Person> index();
+
     public Person show(int id);
+
     public void update(int id, Person updatePerson);
+
     public void save(Person person);
+
     public void delete(int id);
 }
--- src/main/java/web/service/UserService.java
+++ src/main/java/web/service/UserService.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface UserService {
     public List<Person> index();
+
     public Person show(int id);
+
     public void update(int id, Person updatePerson);
+
     public void save(Person person);
+
     public void delete(int id);
 }
